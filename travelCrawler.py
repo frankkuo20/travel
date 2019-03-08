e0f8a14c67f7dee618b20179aabccffc8c9d16eb
frankchih <frankkuo20@gmail.com>
2019-03-08 15:05:49 +0800

    update refactor

--- travelCrawler.py
+++ travelCrawler.py
@@ -5,127 +5,97 @@ import requests
 
 
 class Base:
-    def __init__(self, page=1, endPage=1):
+    def __init__(self, host='', page=1, endPage=1):
+        self._host = host
         self._page = page
         self._endPage = endPage + 1
         self._travelData = []  # 旅遊資料
-        self._filghtData = []  # 航班資料
+        self._flightData = []  # 航班資料
 
     def run(self):
         for i in range(self._page, self._endPage):
             self.crawler(i)
 
-    def crawler(self, page):  # overridde
-        raise NotImplementedError("Must override")
+    def crawler(self, page):
+
+        url = '{}/EW/Services/SearchListData.asp'.format(self._host)
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        data = {
+            'displayType': 'G', 'subCd': '', 'orderCd': '',
+            'pageALL': page,  # 分頁
+            'pageGO': '1', 'pagePGO': '1', 'waitData': 'false', 'waitPage': 'false', 'mGrupCd': '',
+            'SrcCls': '', 'tabList': '', 'regmCd': '', 'regsCd': '', 'beginDt': '2019/03/07',
+            'endDt': '2019/09/07', 'portCd': '', 'tdays': '', 'bjt': '', 'carr': '',
+            'allowJoin': '1', 'allowWait': '1', 'ikeyword': '',
+        }
+        res = requests.post(url, headers=headers, data=data)
+        res = res.json()
+        # res['All'] #全部商品, res['Go'] # 團體旅遊, res['Pgo'] #團體自由行
+        siteTitle = res['SiteTitle']
+
+        for obj in res['All']:
+            row = [siteTitle,
+                   obj['GrupCd'], obj['GrupLn'], obj['GrupSnm'], obj['LeavDt'],
+                   obj['SaleAm'], obj['SaleYqt'], obj['EstmTotqt'],
+                   obj['WeekDay'], obj['FullSts'], obj['SignUpLink'], obj['IsShowGuarantee'],
+                   obj['IsShowPromote'], obj['IsShowHotTp'], obj['SubCdAnm'], obj['Url'], obj['ImgUrl']]
+            self._travelData.append(row)
+            print(siteTitle, obj['GrupCd'])
+            self.filghtCrawler(siteTitle, obj['GrupCd'], obj['SacctNo'])
+
+    def filghtCrawler(self, siteTitle, prodCd, sacctNo):
+        url = '{}/EW/Services/SearchFlight.asp?prodCd={}&sacctNo={}&flightType=1'
+        url = url.format(self._host, prodCd, sacctNo)
+
+        res = requests.get(url)
+        res = res.json()
+        for obj in res['Flights']:
+            row = [
+                siteTitle,
+                obj['ProdCd'], obj['GbookDy'], obj['FltNo'],
+                obj['DepDt'], obj['DepTm'], obj['ArrDt'],
+                obj['ArrTm'], obj['RoutId'], obj['DepCityNm'],
+                obj['ArrCityNm'], obj['DepAirpNm'], obj['ArrAirpNm'],
+                obj['CarrNm'],
+            ]
+            self._flightData.append(row)
 
     @property
     def travelData(self):
         return self._travelData
 
     @property
-    def filghtData(self):
-        return self._filghtData
+    def flightData(self):
+        return self._flightData
 
 
 class OrangeTravel(Base):
     def __init__(self, page=1, endPage=1):
-        super().__init__(page, endPage)
-
-    def crawler(self, page):
-        url = 'http://www.orangetour.com.tw/EW/Services/SearchListData.asp'
-        headers = {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-        data = {
-            'displayType': 'G', 'subCd': '', 'orderCd': '',
-            'pageALL': page,  # 分頁
-            'pageGO': '1', 'pagePGO': '1', 'waitData': 'false', 'waitPage': 'false', 'mGrupCd': '',
-            'SrcCls': '', 'tabList': '', 'regmCd': '', 'regsCd': '', 'beginDt': '2019/03/07',
-            'endDt': '2019/09/07', 'portCd': '', 'tdays': '', 'bjt': '', 'carr': '',
-            'allowJoin': '1', 'allowWait': '1', 'ikeyword': '',
-        }
-        res = requests.post(url, headers=headers, data=data)
-        res = res.json()
-        # res['All'] #全部商品, res['Go'] # 團體旅遊, res['Pgo'] #團體自由行
-        siteTitle = res['SiteTitle']
-
-        for obj in res['All']:
-            row = [siteTitle,
-                   obj['GrupCd'], obj['GrupLn'], obj['GrupSnm'], obj['LeavDt'],
-                   obj['SaleAm'], obj['SaleYqt'], obj['EstmTotqt'],
-                   obj['WeekDay'], obj['FullSts'], obj['SignUpLink'], obj['IsShowGuarantee'],
-                   obj['IsShowPromote'], obj['IsShowHotTp'], obj['SubCdAnm'], obj['Url'], obj['ImgUrl']]
-            print(siteTitle, obj['GrupCd'])
-            self._travelData.append(row)
-            self.filghtCrawler(siteTitle, obj['GrupCd'], obj['SacctNo'])
-
-    def filghtCrawler(self, siteTitle, prodCd, sacctNo):
-        url = 'http://www.orangetour.com.tw/EW/Services/SearchFlight.asp?prodCd={}&sacctNo={}&flightType=1'
-        url = url.format(prodCd, sacctNo)
-
-        res = requests.get(url)
-        res = res.json()
-        for obj in res['Flights']:
-            row = [
-                siteTitle,
-                obj['ProdCd'], obj['GbookDy'], obj['FltNo'],
-                obj['DepDt'], obj['DepTm'], obj['ArrDt'],
-                obj['ArrTm'], obj['RoutId'], obj['DepCityNm'],
-                obj['ArrCityNm'], obj['DepAirpNm'], obj['ArrAirpNm'],
-                obj['CarrNm'],
-            ]
-            self._filghtData.append(row)
+        host = 'http://www.orangetour.com.tw'
+        super().__init__(host, page, endPage)
 
 
 class NewamazingTravel(Base):
     def __init__(self, page=1, endPage=1):
-        super().__init__(page, endPage)
+        host = 'https://www.newamazing.com.tw'
+        super().__init__(host, page, endPage)
 
-    def crawler(self, page):
 
-        url = 'https://www.newamazing.com.tw/EW/Services/SearchListData.asp'
-        headers = {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-        data = {
-            'displayType': 'G', 'subCd': '', 'orderCd': '',
-            'pageALL': page,  # 分頁
-            'pageGO': '1', 'pagePGO': '1', 'waitData': 'false', 'waitPage': 'false', 'mGrupCd': '',
-            'SrcCls': '', 'tabList': '', 'regmCd': '', 'regsCd': '', 'beginDt': '2019/03/07',
-            'endDt': '2019/09/07', 'portCd': '', 'tdays': '', 'bjt': '', 'carr': '',
-            'allowJoin': '1', 'allowWait': '1', 'ikeyword': '',
-        }
-        res = requests.post(url, headers=headers, data=data)
-        res = res.json()
-        # res['All'] #全部商品, res['Go'] # 團體旅遊, res['Pgo'] #團體自由行
-        siteTitle = res['SiteTitle']
+class SaveCsv:
+    def __init__(self, csvPath):
+        self._csvFile = open(csvPath, 'w', newline='')
+        self._csv_file_writer = csv.writer(self._csvFile)
 
-        for obj in res['All']:
-            row = [siteTitle,
-                   obj['GrupCd'], obj['GrupLn'], obj['GrupSnm'], obj['LeavDt'],
-                   obj['SaleAm'], obj['SaleYqt'], obj['EstmTotqt'],
-                   obj['WeekDay'], obj['FullSts'], obj['SignUpLink'], obj['IsShowGuarantee'],
-                   obj['IsShowPromote'], obj['IsShowHotTp'], obj['SubCdAnm'], obj['Url'], obj['ImgUrl']]
-            self._travelData.append(row)
-            print(siteTitle, obj['GrupCd'])
-            self.filghtCrawler(siteTitle, obj['GrupCd'], obj['SacctNo'])
+    def writeRow(self, row):
+        self._csv_file_writer.writerow(row)
 
-    def filghtCrawler(self, siteTitle, prodCd, sacctNo):
-        url = 'https://www.newamazing.com.tw/EW/Services/SearchFlight.asp?prodCd={}&sacctNo={}&flightType=1'
-        url = url.format(prodCd, sacctNo)
+    def writeRows(self, rowList):
+        self._csv_file_writer.writerows(rowList)
 
-        res = requests.get(url)
-        res = res.json()
-        for obj in res['Flights']:
-            row = [
-                siteTitle,
-                obj['ProdCd'], obj['GbookDy'], obj['FltNo'],
-                obj['DepDt'], obj['DepTm'], obj['ArrDt'],
-                obj['ArrTm'], obj['RoutId'], obj['DepCityNm'],
-                obj['ArrCityNm'], obj['DepAirpNm'], obj['ArrAirpNm'],
-                obj['CarrNm'],
-            ]
-            self._filghtData.append(row)
+    def close(self):
+        self._csvFile.close()
 
 
 class MainApp:
@@ -142,12 +112,11 @@ class MainApp:
                       '價錢', '可售位', '總團位',
                       '星期', '備註', '報名狀態', '是否保證出團',
                       '是否促銷', '是否額滿', '類型', '旅程連結', '封面圖片連結']
-        csvFile = open(travelCsvPath, 'w', newline='')
-        csv_file_writer = csv.writer(csvFile)
-        csv_file_writer.writerow(travelHead)
-        csv_file_writer.writerows(orangeTravel.travelData)
-        csv_file_writer.writerows(newamazingTravel.travelData)
-        csvFile.close()
+        saveCsv = SaveCsv(travelCsvPath)
+        saveCsv.writeRow(travelHead)
+        saveCsv.writeRows(orangeTravel.travelData)
+        saveCsv.writeRows(newamazingTravel.travelData)
+        saveCsv.close()
 
         flightCsvPath = 'flight.csv'
         flightHead = ['旅行社',
@@ -155,12 +124,11 @@ class MainApp:
                       '起飛時間', '抵達日期', '抵達時間',
                       'RoutId', '出發城市', '抵達城市', '出發地',
                       '目的地', '航空公司']
-        csvFile = open(flightCsvPath, 'w', newline='')
-        csv_file_writer = csv.writer(csvFile)
-        csv_file_writer.writerow(flightHead)
-        csv_file_writer.writerows(orangeTravel.filghtData)
-        csv_file_writer.writerows(newamazingTravel.filghtData)
-        csvFile.close()
+        saveCsv = SaveCsv(flightCsvPath)
+        saveCsv.writeRow(flightHead)
+        saveCsv.writeRows(orangeTravel.flightData)
+        saveCsv.writeRows(newamazingTravel.flightData)
+        saveCsv.close()
 
 
 if __name__ == '__main__':
@@ -173,8 +141,6 @@ if __name__ == '__main__':
         endPage = int(sys.argv[2])
 
     MainApp(page, endPage)
-
-
 
 # {
 #     "GrupCd": "EE1090309QRA", # id unique
